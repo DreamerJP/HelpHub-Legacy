15c511b7bb195011ce8f8827e1a1813d22c054c0
Paulo Gama <DreamerJPMG@gmail.com>
2025-03-29 23:08:15 -0300

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -32,8 +32,5 @@ echo "==========================================="
 echo "Server logs: logs/gunicorn_access.log e logs/gunicorn_error.log"
 echo ""
 
-# Garante que o diretório de logs existe antes de iniciar o Gunicorn
-mkdir -p logs
-
 # Inicia o Gunicorn
 gunicorn -c gunicorn_config.py app:app
